526a547b9904c5ff1e0a4e4ba5efbcb2d1a9b488
dlscoccia <2054@holbertonschool.com>
2020-08-12 19:31:26 +0000

    Adding holberton.h v1.0

---
+++ holberton.h
@@ -0,0 +1,14 @@
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
+
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define BUFSIZE 64
+#define DELIM " \t\r\n\a"
+
+
+#endif /* HOLBERTON */
